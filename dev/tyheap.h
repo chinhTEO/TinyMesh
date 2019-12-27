cb3c4bd745dcbb5dca2f6cbd54f3e4f2dd3fb8a0
root <mrtruongchinhnguyen@gmail.com>
2019-12-27 15:34:12 +0700

    first test successfull

--- dev/tyheap.h
+++ dev/tyheap.h
@@ -1,5 +1,8 @@
 #ifndef TYHEAP_H_
 #define TYHEAP_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include <stddef.h>
 
@@ -9,4 +12,7 @@ void  tyheap_free( void *ptr );
 
 void  tyheap_organize(void );
 
+#ifdef __cplusplus
+} // closing brace for extern "C"
 #endif
+#endif //TYHEAP_H_
---
+++ test/dev/tyheap_utest.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+#include "tyheap.h"
+
+TEST(tyheap_test, test_alloc){
+}
