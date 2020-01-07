02d514c07ff8125c6db71ca526d3332b367e597d
root <mrtruongchinhnguyen@gmail.com>
2020-01-07 22:12:18 +0700

    fix test pipe with BUILD UTEST OFF

--- net/message.c
+++ net/message.c
@@ -2,18 +2,18 @@
 #include "message_fixed.h"
 #include "message_variable.h"
 
-unsigned short message_process_list(uint8_t *data, unsigned short len){
-    unsigned short pos = 0;
-    unsigned short package_len = 0;
-    do{
-        package_len = message_process(&data[pos]);
-        pos += package_len;
-    }while(pos < len - 1);
+
+enum MESSAGE_LENGTH_TYPE getTypeOfMessage(uint8_t *data){
+    if((*data & 0x01) == VARIABLE){
+        return VARIABLE;
+    }else{
+        return FIXED;
+    }
 }
 
 unsigned short message_process(uint8_t *data){
     unsigned short len;
-    if(package_getGeneralType(data) == FIXED){
+    if(getTypeOfMessage(data) == FIXED){
         len = message_fixed_proccess(data);
     }else{
         len = message_variable_proccess(data);
@@ -22,10 +22,12 @@ unsigned short message_process(uint8_t *data){
 
 }
 
-enum MESSAGE_LENGTH_TYPE getTypeOfMessage(uint8_t *data){
-    if((*data & 0x01) == VARIABLE){
-        return VARIABLE;
-    }else{
-        return FIXED;
-    }
+
+unsigned short message_process_list(uint8_t *data, unsigned short len){
+    unsigned short pos = 0;
+    unsigned short package_len = 0;
+    do{
+        package_len = message_process(&data[pos]);
+        pos += package_len;
+    }while(pos < len - 1);
 }
