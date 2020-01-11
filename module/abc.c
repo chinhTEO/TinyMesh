7668c6ecc6b8f06b8290282e60ab1dca048fb7f3
root <mrtruongchinhnguyen@gmail.com>
2020-01-11 22:21:46 +0700

    done abc

--- module/abc.c
+++ module/abc.c
@@ -1,11 +1,18 @@
 #include "abc.h"
 #include "message_variable.h"
 #include "message.h"
+#include "tymesh_framer.h"
+#include "string.h"
+#include <stdio.h>
 
 void abc_send(uint8_t *data, unsigned len){
-    
+    struct Message_out *message = message_variable_create(MESSAGE_ABC, data, len, HIGH_PRIORITY);
+    tymesh_framer_add(message);
 }
 
 unsigned short abc_callback(uint8_t *data, unsigned short len){
-
+    char buffer[50];
+    memcpy(buffer, data, len);
+    buffer[len] = '\0';
+    printf("abc recv :%s\n", buffer);
 }
