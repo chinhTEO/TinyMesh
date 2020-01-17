bdbad36b6f5de5ce20192337414f0f8214ba0d11
root <mrtruongchinhnguyen@gmail.com>
2020-01-17 14:04:58 +0700

    change cmake stucture

--- platform/native/platform_def.h
+++ platform/native/platform_def.h
@@ -1,6 +1,12 @@
 #ifndef NATIVE_DEF_H_
 #define NATIVE_DEF_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef unsigned long clock_time_t;
 
+#ifdef __cplusplus
+} // closing brace for extern "C"
+#endif
 #endif //NATIVE_DEF_H_
